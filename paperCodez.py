0e473ba51d91a0aba8562809178202705b163c93
dru93 <33983402+dru93@users.noreply.github.com>
2018-11-08 04:08:13 +0100

    Add files via upload

--- paperCodez.py
+++ paperCodez.py
@@ -51,20 +51,84 @@ end = time.time()
 elapsedClosenessApprox = end - start
 
 # average graph betweenness approximation
+# calculate distance from pivot to all nodes t
+# betweenness of u = to sum(sppps from pivot to t that contain u)/sum(different shortest pivot-u paths)
+
+
+def _single_source_shortest_path_basic(G, s):
+        S = []
+        P = {}
+        for v in G:
+                P[v] = []
+        sigma = dict.fromkeys(G, 0.0)    # sigma[v]=0 for v in G
+        D = {}
+        sigma[s] = 1.0
+        D[s] = 0
+        Q = [s]
+        while Q:   # use BFS to find shortest paths
+                v = Q.pop(0)
+                S.append(v)
+                Dv = D[v]
+                sigmav = sigma[v]
+                for w in G[v]:
+                        if w not in D:
+                                Q.append(w)
+                                D[w] = Dv + 1
+                        if D[w] == Dv + 1:   # this is a shortest path, count paths
+                                sigma[w] += sigmav
+                                P[w].append(v)  # predecessors
+        return S, P, sigma
+
+def _accumulate_endpoints(betweenness, S, P, sigma, s):
+    betweenness[s] += len(S) - 1
+    delta = dict.fromkeys(S, 0)
+    while S:
+        w = S.pop()
+        coeff = (1 + delta[w]) / sigma[w]
+        for v in P[w]:
+            delta[v] += sigma[v] * coeff
+        if w != s:
+            betweenness[w] += delta[w] + 1
+    return betweenness
+
+def _rescale(betweenness, n, normalized,
+             directed=False, k=None, endpoints=False):
+    if normalized:
+        if endpoints:
+            if n < 2:
+                scale = None  # no normalization
+            else:
+                # Scale factor should include endpoint nodes
+                scale = 1 / (n * (n - 1))
+        elif n <= 2:
+            scale = None  # no normalization b=0 for all nodes
+        else:
+            scale = 1 / ((n - 1) * (n - 2))
+    else:  # rescale by 2 for undirected graphs
+        if not directed:
+            scale = 0.5
+        else:
+            scale = None
+    if scale is not None:
+        if k is not None:
+            scale = scale * n / k
+        for v in betweenness:
+            betweenness[v] *= scale
+    return betweenness
+
 start = time.time()
 
-for pivot in pivots:
-        pathsNumber = 0
-        pathsNumberWithPivot = 0
-        nodeBetweenness = []
-        for n in G:
-                path = nx.shortest_path(G, source = pivot, target = n)
-                for pathIndex in range (0, len(path)-1):
-                        if path[pathIndex] == pivot:
-                                pathsNumberWithPivot += 1
-                pathsNumber += 1
-        nodeBetweenness.append(pathsNumberWithPivot/pathsNumber)
+betweenness = dict.fromkeys(G, 0.0)  # b[v]=0 for v in G
+nodes = pivots
+for s in nodes:
+        S, P, sigma = _single_source_shortest_path_basic(G, s)
+        betweenness = _accumulate_endpoints(betweenness, S, P, sigma, s)
+        betweenness = _rescale(betweenness, len(G), normalized=True,
+                           directed=False, k=None, endpoints=False)
 
-averageBetweennessApprox = statistics.mean(nodeBetweenness)
+averageBetweennessApprox = statistics.mean(betweenness.values())
 end = time.time()
 elapsedBetweennessApprox = end - start
+
+print(elapsedBetweenness, elapsedCloseness, elapsedBetweennessApprox, elapsedClosenessApprox)
+print(averageBetweenness, averageBetweennessApprox, averageCloseness, averageClosenessApprox)
